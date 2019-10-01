d3503faefb44487d516a72581f68fe4a6f2f25a3
masterworgen <melkitasit@yandex.ru>
2019-10-01 21:46:57 +0500

    init

---
+++ API/WebSite/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Controllers
+{
+    [ApiController]
+    [Route("main")]
+    public class MainController : ControllerBase
+    {
+        public MainController()
+        {
+
+        }
+
+        [Route("test")]
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "HelloWorld";
+        }
+    }
+}
