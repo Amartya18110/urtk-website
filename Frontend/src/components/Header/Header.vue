7eb6d9f6fc21687926b957218d35430f2bb71afb
Dmitry Kulakov <dmitry.kulakov.00@gmail.com>
2019-10-16 01:52:03 +0500

    Выделение меню навигации в отдельный компонент

--- Frontend/src/components/Header/Header.vue
+++ Frontend/src/components/Header/Header.vue
@@ -1,18 +1,15 @@
 <template>
   <header>
     <div class="logo">logo</div>
-    <div class="menu">
-      <ul>
-        <li><router-link to="/home">Главная</router-link></li>
-        <li><router-link to="/about">О проекте</router-link></li>
-      </ul>
-    </div>
+    <NavigationMenu></NavigationMenu>
   </header>
 </template>
 
 <script>
+  import NavigationMenu from "../NavigationMenu/NavigationMenu.vue";
   export default {
-    name: "Header"
+    name: "Header",
+    components: {NavigationMenu}
   }
 </script>
 
@@ -29,40 +26,5 @@
 
   .logo {
     color: white;
-  }
-
-  .menu {
-    // TODO: выделить меню в одтельный компонент
-    $menu-button-padding: 10px;
-    $menu-button-underline-height: 3px;
-
-    ul {
-      margin: 0;
-      padding: 0;
-      display: flex;
-    }
-
-    li {
-      list-style-type: none;
-      font-weight: bold;
-    }
-
-    a {
-      padding: $menu-button-padding;
-      display: block;
-      color: white;
-      transition: 0.2s;
-    }
-
-    a:active {
-      color: $primary-color;
-      background: white;
-      border-radius: 5px;
-    }
-
-    a.router-link-active {
-      padding-bottom: $menu-button-padding - $menu-button-underline-height;
-      border-bottom: $menu-button-underline-height solid white;
-    }
   }
 </style>
---
+++ Frontend/src/components/NavigationMenu/NavigationMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="menu">
+    <ul>
+      <li><router-link to="/home">Главная</router-link></li>
+      <li><router-link to="/about">О проекте</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavigationMenu"
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "common.scss";
+
+  $menu-button-padding: 10px;
+  $menu-button-underline-height: 3px;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  li {
+    list-style-type: none;
+    font-weight: bold;
+  }
+
+  a {
+    padding: $menu-button-padding;
+    display: block;
+    color: white;
+    transition: 0.2s;
+  }
+
+  a:active {
+    color: $primary-color;
+    background: white;
+    border-radius: 5px;
+  }
+
+  a.router-link-active {
+    padding-bottom: $menu-button-padding - $menu-button-underline-height;
+    border-bottom: $menu-button-underline-height solid white;
+  }
+</style>
